f04d5a76fcab89afb64b16a0718bfc9b6f4a12b6
Abdulrahman Mohammed <abdulrahman48811@gmail.com>
2023-04-14 11:11:51 -0700

    PokeDex 3

--- main.js
+++ main.js
@@ -2,14 +2,37 @@ const baseAPI = "https://pokeapi.co/api/v2/";
 const flexContainer = document.getElementById("flex-container");
 const placeholder = document.getElementById("placeholder");
 
+const addCard = (pokemon) => {
+  const newCard = placeholder.cloneNode(true);
+  const loadingSpinner = newCard.querySelector(".lds-ring");
+
+  loadingSpinner.remove();
+
+  const title = newCard.querySelector(".title");
+  title.innerText = pokemon.name;
+
+  const image = newCard.querySelector("img");
+  image.src = pokemon.sprites.front_default;
+
+  newCard.id = pokemon.id;
+
+  flexContainer.appendChild(newCard);
+};
+
 const getPokemonList = () => {
   fetch(`${baseAPI}pokemon?limit=20&offset=0`)
     .then((res) => res.json())
     .then((data) => {
-      let fetchedPokemon = data.results;
-      console.log(fetchedPokemon);
+      const fetchedPokemon = data.results;
 
-      // more code
+      for (let pokemon of fetchedPokemon) {
+        fetch(pokemon.url)
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data);
+            addCard(data);
+          });
+      }
     });
 };
 
