2a06b32bd60de74cc089e98c3d8cb6c717caef31
Abdulrahman Mohammed <abdulrahman48811@gmail.com>
2023-04-16 14:49:48 -0700

    PokeDex 5

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const baseAPI = "https://pokeapi.co/api/v2/";
 const flexContainer = document.getElementById("flex-container");
 const placeholder = document.getElementById("placeholder");
+const totalPokemon = document.getElementById("total-pokemon");
 
+let page = 0;
 const addCard = (pokemon) => {
   const newCard = placeholder.cloneNode(true);
   const loadingSpinner = newCard.querySelector(".lds-ring");
@@ -20,7 +22,7 @@ const addCard = (pokemon) => {
 };
 const getPokemonList = () => {
     console.time("timer");
-    fetch(`${baseAPI}pokemon?limit=20&offset=0`)
+    fetch(`${baseAPI}pokemon?limit=20&offset=${page * 20}`)
       .then((res) => res.json())
       .then((data) => {
         const fetchedPokemon = data.results;
@@ -39,5 +41,16 @@ const getPokemonList = () => {
         placeholder.classList.add("hidden");
       });
   };
-  
+  const nextPage = () => {
+    showAndMovePlaceholder();
+    placeholder.classList.remove("hidden");
+    page++;
+    getPokemonList();
+    totalPokemon.innerText = (page + 1) * 20;
+  };
+  const showAndMovePlaceholder = () => {
+    placeholder.classList.remove("hidden");
+    placeholder.remove();
+    flexContainer.appendChild(placeholder);
+  };
 getPokemonList();
