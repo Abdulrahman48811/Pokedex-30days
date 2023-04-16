6bb69508bfb7da2640aba00a0ba0206121486d09
Abdulrahman Mohammed <abdulrahman48811@gmail.com>
2023-04-16 14:53:02 -0700

    PokeDex 6

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ const placeholder = document.getElementById("placeholder");
 const totalPokemon = document.getElementById("total-pokemon");
 
 let page = 0;
+let pokemonArray = [];
+
 const addCard = (pokemon) => {
   const newCard = placeholder.cloneNode(true);
   const loadingSpinner = newCard.querySelector(".lds-ring");
@@ -21,36 +23,39 @@ const addCard = (pokemon) => {
   flexContainer.appendChild(newCard);
 };
 const getPokemonList = () => {
-    console.time("timer");
-    fetch(`${baseAPI}pokemon?limit=20&offset=${page * 20}`)
-      .then((res) => res.json())
-      .then((data) => {
-        const fetchedPokemon = data.results;
-  
-        const requests = fetchedPokemon.map((pokemon) => {
-          return fetch(pokemon.url)
-            .then((res) => res.json())
-            .then((data) => {
-              addCard(data);
-            });
-        });
-  
-        return Promise.all(requests);
-      })
-      .finally(() => {
-        placeholder.classList.add("hidden");
+  console.time("timer");
+  fetch(`${baseAPI}pokemon?limit=20&offset=${page * 20}`)
+    .then((res) => res.json())
+    .then((data) => {
+      const fetchedPokemon = data.results;
+
+      const requests = fetchedPokemon.map((pokemon) => {
+        return fetch(pokemon.url)
+          .then((res) => res.json())
+          .then((data) => {
+            pokemonArray.push(data);
+
+            console.log(pokemonArray);
+            addCard(data);
+          });
       });
-  };
-  const nextPage = () => {
-    showAndMovePlaceholder();
-    placeholder.classList.remove("hidden");
-    page++;
-    getPokemonList();
-    totalPokemon.innerText = (page + 1) * 20;
-  };
-  const showAndMovePlaceholder = () => {
-    placeholder.classList.remove("hidden");
-    placeholder.remove();
-    flexContainer.appendChild(placeholder);
-  };
+
+      return Promise.all(requests);
+    })
+    .finally(() => {
+      placeholder.classList.add("hidden");
+    });
+};
+const nextPage = () => {
+  showAndMovePlaceholder();
+  placeholder.classList.remove("hidden");
+  page++;
+  getPokemonList();
+  totalPokemon.innerText = (page + 1) * 20;
+};
+const showAndMovePlaceholder = () => {
+  placeholder.classList.remove("hidden");
+  placeholder.remove();
+  flexContainer.appendChild(placeholder);
+};
 getPokemonList();
